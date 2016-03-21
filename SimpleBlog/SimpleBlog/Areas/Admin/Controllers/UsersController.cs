a2f0787d6f5201c5b7f6ce71078d9620eab8ca6d
Justin Hull <hulljc3@upmc.edu>
2016-03-21 13:56:28 -0400

    Added basic routes and controllers. Added admin area.

---
+++ SimpleBlog/SimpleBlog/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleBlog.Areas.Admin.Controllers
+{
+    public class UsersController : Controller
+    {
+        // http://localhost:55482/admin/users/index
+        // http://localhost:55482/admin/users
+        public ActionResult Index()
+        {
+            return Content("USERS!");
+        }
+    }
+}
